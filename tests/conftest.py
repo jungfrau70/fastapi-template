390d77980a6c3a08d571b7f6e8d18d00437892f5
Ian Jung <inhwan.jung@gmail.com>
2023-02-28 17:22:50 +0900

    Updated

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import json
 import pytest
 
 from fastapi.testclient import TestClient
--- tests/test_itemroute.py
+++ tests/test_itemroute.py
@@ -1,15 +1,15 @@
 from config import setting
 
-def test_create_item(client, header_token):
+def test_create_item(client):
     response = client.post(
         "/items/create/",
-        headers=header_token,
         json={ "title": setting.TITLE, "description": setting.DESCRIPTION}
     )
     assert response.status_code == 202
 
 def test_retrieve_by_id(client):
     response = client.get("/items/1")
+    print(response)
     assert response.status_code == 202
     assert response.json()['title'] == setting.TITLE
     assert response.json()['description'] == setting.DESCRIPTION
